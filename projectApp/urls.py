b2026cf1b10e41f21f8408cd37c04f1f58ece7e1
hashem <has64hem@gmail.com>
2024-05-19 15:07:20 +0300

    add animation and som style

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -7,6 +7,14 @@ urlpatterns = [
     path('signup/',views.signup, name='signup'),
     path('items/products',views.products, name='products'),
     path('item/product/add',views.add , name='add'),
-      path('item/product/update',views.update , name='update'),
+    path('item/product/update',views.update , name='update'),
+    path('item/product/update/<int:product_id>',views.update , name='updateProduct'),
+    path('item/product/<int:product_id>',views.product , name='product'),
+    path('logout/', views.userLogout, name='userLogout'),
+    path('delete/<int:product_id>/', views.delete, name='delete'),
+    path('delete-purchase/<int:purchase_id>/', views.deleteFromHistory, name='delete-purchase'),
+    path('purchase/<int:product_id>/<str:status>', views.purchase, name='purchase'),
+    path('history', views.history, name='history'),
+    path('updated-purchase/<int:purchase_id>', views.updatedPurchase, name='updated-purchase'),
 
 ]
